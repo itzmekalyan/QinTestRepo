34d81f73aac33ac7658166bc57500f1e97704049
Kalyan Moturu <moturukalyan@gmail.com>
2018-02-08 16:10:11 -0600

    added files

---
+++ app/src/main/java/com/tyorikan/awscodebuild/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tyorikan.awscodebuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	//test
+    	int i = 0;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
